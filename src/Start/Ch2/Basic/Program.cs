8c11f5f1bf3f155b11da8c27584ede05a74f5c71
Edwin Choate <edwin.choate@pm.me>
2024-09-23 13:27:40 -0400

    Basic exception handling code

--- src/Start/Ch2/Basic/Program.cs
+++ src/Start/Ch2/Basic/Program.cs
@@ -1,15 +1,30 @@
 ﻿// Exercise file for C# Exception and Error Handling by Joe Marini
 // Basic exceptions
 
-int[] nums = {0,1,2,3,4,5,6,7,8,9};
+try 
+{
+    int[] nums = {0,1,2,3,4,5,6,7,8,9};
 
-// Dividing by zero causes an exception
-Console.WriteLine($"Result: {IntDivider(nums[5],nums[2])}");
+    // Dividing by zero causes an exception
+    Console.WriteLine($"Result: {IntDivider(nums[5],nums[0])}");
 
-// Indexing an array out of bounds is also an exception
-Console.WriteLine($"{nums[8]}");
+    // Indexing an array out of bounds is also an exception
+    Console.WriteLine($"{nums[18]}");
 
-// A funtion that divides two integers
-int IntDivider(int a, int b) {
-    return a / b;
+    // A funtion that divides two integers
+    int IntDivider(int a, int b) {
+        return a / b;
+    }
+}
+catch(DivideByZeroException e) 
+{
+    Console.WriteLine(e.Message);
+}
+catch(IndexOutOfRangeException e)
+{
+    Console.WriteLine(e.Message);
+}
+catch(Exception e) 
+{
+    Console.WriteLine(e.Message);
 }
