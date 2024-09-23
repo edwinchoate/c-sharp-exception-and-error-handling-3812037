4d2d5e31a7fd547de61dfaded27e319572c874bb
Edwin Choate <edwin.choate@pm.me>
2024-09-23 17:30:28 -0400

    2.6 Challenge: Basics

--- src/Start/Ch2/ChallengeConvert/Program.cs
+++ src/Start/Ch2/ChallengeConvert/Program.cs
@@ -5,17 +5,56 @@ bool Continue = true;
 
 while (Continue) {
     Console.WriteLine("Enter the first integer number: ");
-    string? str = Console.ReadLine();
+    string str = Console.ReadLine() ?? "";
     Console.WriteLine("Enter the second integer number: ");
-    string? str2 = Console.ReadLine();
+    string str2 = Console.ReadLine() ?? "";
 
     Console.WriteLine("Enter the operation: (a)dd, (s)ubtract, (m)ultiply, (d)ivide or 'exit' to quit: ");
-    string? op = Console.ReadLine();
-    op = op?.ToLower();
+    string op = Console.ReadLine() ?? "";
+    op = op.ToLower();
     if (op == "exit") {
         break; // exit the loop at this point and terminate the program
     }
 
-    // Challenge: Convert the string to a number and perform the operation
+    int n1, n2;
 
+    // Challenge: Convert the string to a number and perform the operation
+    try
+    {
+        n1 = Int32.Parse(str);
+        n2 = Int32.Parse(str2);
+
+        Console.WriteLine($"Result: {calculate(n1, n2, op)}");
+    }
+    catch (Exception e) 
+    {
+        Console.WriteLine(e.Message);
+    }
+
+}
+
+
+static int calculate (int a, int b, string op)
+{
+    int x = 0;
+
+    switch (op)
+    {
+        case "a":
+            x = a + b;
+            break;
+        case "s":
+            x = a - b;
+            break;
+        case "m":
+            x = a * b;
+            break;
+        case "d":
+            x = a / b;
+            break;
+        default: 
+            throw new InvalidOperationException();
+    }
+
+    return x;
 }
